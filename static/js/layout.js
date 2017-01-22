35709dc7c3f709dc3fecdcb15bd7177c1c9029bd
2DU <min08101@naver.com>
2017-01-22 21:02:00 +0900

    Update layout.js

--- static/js/layout.js
+++ static/js/layout.js
@@ -14,5 +14,16 @@ jQuery('.btn-group').on('show.bs.dropdown', function(e) {
 jQuery('.btn-group').on('hide.bs.dropdown', function(e) {
     jQuery(this).find('.dropdown-menu').first().stop(true, true).fadeToggle(200);
 });
+
+$(".wiki-article .wiki-fn-content").click(function(){
+    var target=$(this).attr('href');
+    target=target.replace("#","");
+    var $tmp=$("span[class=target][id=\""+ target+"\"]").parent().clone();
+    $tmp.children("span[class=target]").remove();
+    $tmp.children("a[href^='#rfn-']").remove();
+    $("#footnoteModal .modal-header").html("<h5 class=\"modal-title\">각주: "+ $(this).text()+"</h5>");
+    $("#footnoteModal .modal-body").html($tmp.html());$("#footnoteModal").modal('show');
+    return false;
+});
 /* 드롭다운 페이드인 End */
 
