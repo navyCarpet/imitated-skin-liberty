c4511839ee19885da5006e28610483c6e83c85ef
navyCarpet <125118599+navyCarpet@users.noreply.github.com>
2023-02-21 19:52:20 +0900

    Fix star button syntax

--- layout.vue
+++ layout.vue
@@ -106,32 +106,18 @@
             </div>
             <div class="container-fluid liberty-content">
                 <div class="liberty-content-header">
-                    <div class="content-tools" v-if="$store.state.page.viewName === 'wiki'">
+                    <div class="content-tools" v-if="$store.state.page.viewName === 'wiki' || $store.state.page.viewName === 'notfound'">
                         <div class="btn-group" role="group" aria-label="content-tools">
                             <nuxt-link v-if="$store.state.page.data.starred"
                                         :to="doc_action_link($store.state.page.data.document, 'member/unstar')" class="btn btn-secondary tools-btn">
                                 <span class="fa fa-star"></span>
                                 <span class="star-count">{{ $store.state.page.data.star_count }}</span>
                             </nuxt-link>
-                            <nuxt-link v-else-if="$store.state.page.viewName === 'wiki' && $store.state.page.data.star_count !== null"
+                            <nuxt-link v-else-if="$store.state.page.data.star_count >= 0"
                                              :to="doc_action_link($store.state.page.data.document, 'member/star')" class="btn btn-secondary tools-btn">
                                 <span class="fa fa-star-o"></span>
                                 <span class="star-count">{{ $store.state.page.data.star_count }}</span>
                             </nuxt-link>
-                            <nuxt-link :to="doc_action_link($store.state.page.data.document, 'backlink')" class="btn btn-secondary tools-btn">역링크</nuxt-link>
-                            <nuxt-link :to="doc_action_link($store.state.page.data.document, 'discuss')"  class="btn btn-secondary tools-btn" v-bind:class="{ 'btn-discuss-progress': $store.state.page.data.discuss_progress }">토론</nuxt-link>
-                            <nuxt-link :to="doc_action_link($store.state.page.data.document, 'edit')" class="btn btn-secondary tools-btn">편집</nuxt-link>
-                            <nuxt-link :to="doc_action_link($store.state.page.data.document, 'history')"  class="btn btn-secondary tools-btn">역사</nuxt-link>
-                            <nuxt-link v-if="$store.state.page.data.user"
-                                            :to="contribution_author_link($store.state.page.data.document.title)" class="btn btn-secondary tools-btn">기여</nuxt-link>
-                            <nuxt-link :to="doc_action_link($store.state.page.data.document, 'acl')"  class="btn btn-secondary tools-btn">ACL</nuxt-link>
-                            <template v-if="$store.state.page.data.menus">
-                                <nuxt-link v-for="m in $store.state.page.data.menus" v-bind:key="m.to" :to="m.to" class="btn btn-secondary tools-btn" v-text="m.title" />
-                            </template>
-                        </div>
-                    </div>
-                    <div class="content-tools" v-else-if="$store.state.page.viewName === 'notfound'">
-                        <div class="btn-group" role="group" aria-label="content-tools">
                             <nuxt-link :to="doc_action_link($store.state.page.data.document, 'backlink')" class="btn btn-secondary tools-btn">역링크</nuxt-link>
                             <nuxt-link :to="doc_action_link($store.state.page.data.document, 'discuss')"  class="btn btn-secondary tools-btn" v-bind:class="{ 'btn-discuss-progress': $store.state.page.data.discuss_progress }">토론</nuxt-link>
                             <nuxt-link :to="doc_action_link($store.state.page.data.document, 'edit')" class="btn btn-secondary tools-btn">편집</nuxt-link>
