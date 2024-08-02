5768139913a049bb0d4e82c7a800118053fda155
navyCarpet <125118599+navyCarpet@users.noreply.github.com>
2024-08-02 09:10:39 +0900

    Fix AuthorSpan style

--- static/js/AuthorSpan.js
+++ static/js/AuthorSpan.js
@@ -1,18 +1,66 @@
 window.onload = function() {
     document.querySelectorAll("a[href^='/w/사용자:']").forEach(item => {
         const popover = document.createElement('div');
+        popover.className = 'author-span';
         popover.style.display = 'none';
         popover.style.zIndex = 9999;
         popover.style.position = 'absolute';
         popover.style.top = '25px';
         popover.style.right = '0px';
         popover.innerHTML = `
-        <div class="tooltip-inner popover-inner" style="align-items: center;display: flex;gap: 0.5rem;position: relative; background: #f9f9f9; border-radius: 5px; box-shadow: 0 5px 30px rgba(0, 0, 0, .2); color: #000; padding: 16px;">
-            <a href="/contribution/author/${item.textContent}/document" class="btn btn-secondary btn-sm">기여내역</a><a href="${item.href}" class="btn btn-primary btn-sm">사용자 문서</a>
-        </div>
-        <div class="tooltip-arrow popover-arrow" style="border-style: solid; height: 0; margin: 5px; position: absolute; width: 0; z-index: 1; border-width: 0 5px 5px; right: 5px; margin-bottom: 0; margin-top: 0; top: -5px; border-left-color: transparent !important; border-right-color: transparent !important; border-top-color: transparent !important; border-color: #f9f9f9;"></div>`;
-        item.setAttribute('onclick', "$(this).next().fadeToggle('fast'); return false;");
-        item.parentElement.style.position = 'relative'
+            <div class="popper">
+                <div style="padding: .25rem 1rem;">
+                    <div style="font-size: .8rem;">사용자</div>
+                    <div style="font-size: .95rem; font-weight: 600; line-height: 1; margin: 0 0 .3rem;">${item.textContent}</div>
+                </div>
+                <hr style="border: 0; border-top: 1px solid #d5d5d5; margin: .25rem 0;">
+                <a href="${item.href}" class="popper_btn" role="button">사용자 문서</a>
+                <hr style="border: 0; border-top: 1px solid #d5d5d5; margin: .25rem 0;">
+                <a href="/contribution/author/${item.textContent}/document" class="popper_btn" role="button">문서 기여 내역</a>
+                <a href="/contribution/author/${item.textContent}/discuss" class="popper_btn" role="button">토론 기여 내역</a>
+            </div>`;
+        item.setAttribute('onclick', `$('.author-span').hide(); $(this).next().fadeToggle('fast'); return false;`);
+        item.parentElement.style.position = 'relative';
         item.after(popover);
     });
+
+    let style = document.createElement('style')
+    style.innerHTML = `
+        .popper {
+            background-color: #fff;
+            border: 1px solid #d5d5d5;
+            border-radius: 6px;
+            box-shadow: 0 10px 15px -3px rgba(0, 0, 0, .165), 0 4px 6px -4px rgba(0, 0, 0, .165);
+            margin: .25rem 0 0;
+            max-width: 15rem;
+            min-width: 12rem;
+            padding: .25rem 0;
+            word-break: break-all;
+            font-weight: normal;
+        }
+
+        .popper_btn {
+            background-color: #fff;
+            background-color: var(--article-background-color, #fff);
+            border-radius: 4px;
+            color: inherit !important;
+            cursor: pointer;
+            display: inline-flex;
+            font-size: .9rem;
+            line-height: 1.5;
+            overflow: hidden;
+            text-decoration: none !important;
+            transition: background-color .1s ease-in, box-shadow .1s linear;
+            white-space: nowrap;
+            border: none !important;
+            margin: .125rem .25rem;
+            padding: .2rem .75rem !important;
+            display: block;
+        }
+
+        .popper_btn:hover {
+            background-color: #f2f2f2;
+        }
+    `;
+    document.body.appendChild(style);
 }
